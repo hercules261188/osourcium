3d34fc334e5189797906480d8ade12b9f3ae304e
Stanley Owen <69080584+stanleyowen@users.noreply.github.com>
2021-09-27 12:24:24 +0700

    Added Sidebar and Element Components

---
+++ src/components/element.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Data from '../lib/data.json'
+import { Grid, Button } from '@mui/material'
+
+const Element = () => {
+    return (
+        <div>Element</div>
+    )
+}
+
+export default Element
